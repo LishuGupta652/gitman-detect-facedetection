0a91ef28e57b0ebd576bd4473f83e4836ba0d109
lishu <lishugupta652@gmail.com>
2021-12-03 20:28:00 +0530

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <span>Gitman Detect</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
