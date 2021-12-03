d6a2af41767ec6ec8cd29531193ff060dd2b15f1
lishu <lishugupta652@gmail.com>
2021-12-03 20:00:42 +0530

    canvas use-ref

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const imgRef = useRef();
+  const canvasRef = useRef();
   return (
     <div className="App">
       <img
+        ref={imgRef}
         src="https://raw.githubusercontent.com/LishuGupta652/web-static-content/main/lishu%20gupta%20image%2002.jpg"
         alt="test image"
         width="940"
         height="650"
       />
-      <canvas width="940" height="650" />
+      <canvas width="940" height="650" ref={canvasRef} />
     </div>
   );
 }
