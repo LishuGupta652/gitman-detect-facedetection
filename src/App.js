bd87f2e18a8366ef1c9722ffd84df18e5912cf4e
lishu <lishugupta652@gmail.com>
2021-12-03 21:34:51 +0530

    new post

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
 import "./App.css";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import * as faceapi from "face-api.js";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const imgRef = useRef();
-  const canvasRef = useRef();
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
 
-  const handleImage = async () => {
-    const detections = await faceapi
-      .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceExpressions();
-
-    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current);
-    faceapi.matchDimensions(canvasRef.current, {
-      width: 940,
-      height: 650,
-    });
-    const resizedVersion = faceapi.resizeResults(detections, {
-      width: 940,
-      height: 650,
-    });
-    faceapi.draw.drawDetections(canvasRef.current, resizedVersion);
-    faceapi.draw.drawFaceExpressions(canvasRef.current, resizedVersion);
-    faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedVersion);
-    faceapi.draw.drawContour(canvasRef.current, resizedVersion);
-    console.log(detections);
-  };
   useEffect(() => {
-    const loadModels = () => {
-      Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-      ])
-        .then(handleImage)
-        .catch((e) => console.log(e));
+    const getImage = () => {
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      file && console.log();
     };
-    imgRef.current && loadModels();
-  }, []);
+  }, [file]);
+
   return (
     <div>
       <Navbar />
@@ -70,7 +44,12 @@ function App() {
               />
               <button>send</button>
             </label>
-            <input id="file" style={{ display: "none" }} type="file" />
+            <input
+              onChange={(e) => setFile(e.target.files[0])}
+              id="file"
+              style={{ display: "none" }}
+              type="file"
+            />
           </div>
         </div>
       </div>
